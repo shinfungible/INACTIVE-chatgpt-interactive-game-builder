1b932e056d9ed6de508c31b7cd90d9b90d2e2ce7
keishino00 <shin.enginee@gmail.com>
2023-03-16 17:05:42 +0900

    feat: chatgpt api と対話するためのテキスト入力インターフェースの追加

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -4,14 +4,30 @@ import "./App.css";
 
 export const App = () => {
   const [code, setCode] = useState<string>("// ここにTypeScriptコードを書いてください。");
+  const [message, setMessage] = useState<string>("");
 
   const handleCodeChange = (newCode: string) => {
     setCode(newCode);
   };
 
+  const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
   return (
     <div className="container">
-      <CodeContainer code={code} onCodeChange={handleCodeChange} />
+      <div className="left">
+        <CodeContainer code={code} onCodeChange={handleCodeChange} />
+        <div className="chat-input">
+          <input
+            className="text-interface"
+            type="text"
+            value={message}
+            onChange={handleMessageChange}
+            placeholder="ChatGPT APIに入力"
+          />
+        </div>
+      </div>
       <div className="game-container">
         {/* ゲーム画面が表示される領域 */}
       </div>
